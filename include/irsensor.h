06d05f483f3c72f3c11e5cd403f80fc9cc0eea2f
Talal Habbal <talal-habbal@hotmail.com>
2024-02-21 16:11:56 +0100

    IR sensor calibration

--- include/irsensor.h
+++ include/irsensor.h
@@ -5,17 +5,19 @@
 #include <util/delay.h>
 #include <avr/io.h>
 #include <stdlib.h>
-
+#include <Arduino.h>
 //PORT C, pin 0
 #define right_sensor PINC0
 //PORT C, pin 1
 #define left_sensor PINC1
 //PORT C, pin 2
 #define mid_sensor PINC2
+
 #define NUM_OF_SENSORS 3
 
 void initialize_sensors();
 uint8_t analogread(uint8_t channel);
-void readIRsensors(uint8_t* sensorValues);
-
+uint8_t getAverageValue();
+void getLimits(uint8_t channel, uint8_t *min, uint8_t *max);
+float getScaledValue(uint8_t val, uint8_t *min, uint8_t *max);
 #endif
--- src/main.c
+++
@@ -1,9 +0,0 @@
-#include "irsensor.h"
-int main() {
-	initialize_sensors();
-	uint8_t *sensorValues = (uint8_t*)malloc(NUM_OF_SENSORS * sizeof(uint8_t));
-	while(1) {
-		readIRsensors(sensorValues);
-	}
-	return 0;
-}
