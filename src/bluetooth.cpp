b05ca9d9740d6bcb45cb16e2a6292c3b12b63e7b
Liam Boreback <boreback.work@gmail.com>
2024-02-28 14:31:51 +0100

    Finish concept test for bluetooth comms

--- src/bluetooth.cpp
+++ src/bluetooth.cpp
@@ -1,10 +1,39 @@
 #include <avr/io.h>
+#include <avr/interrupt.h>
+
+#define BAUD 9600
+#include <util/setbaud.h>
 
 void initialise_bluetooth()
 {
 	// Baud Rate
-	// UBRR0 = 103;
-	UBRR0 = 25;
+	UBRR0L = UBRRL_VALUE;
+	UBRR0H = UBRRH_VALUE;
 
-	UCSR0B = 0b1 << TXEN0 | 0b1 << RXCIE0 | 0b1 << RXEN0;
+	UCSR0B = 0b1 << TXEN0 | 0b1 << TXCIE0 | 0b1 << RXCIE0 | 0b1 << RXEN0;
+
+	UCSR0C = 0b11 << UCSZ00;
+
+	DDRB = 0xff;
+}
+
+ISR(USART_RX_vect)
+{
+	char in = UDR0;
+
+	if (in == '1')
+	{
+		PORTB = 0xff;
+
+		while (!(UCSR0A & (1 << UDRE0)))
+			;
+		UDR0 = 'y';
+	}
+	else if (in == '0')
+	{
+		PORTB = 0x00;
+		while (!(UCSR0A & (1 << UDRE0)))
+			;
+		UDR0 = 'n';
+	}
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,59 +1,15 @@
-#include <avr/delay.h>
 #include <avr/interrupt.h>
-
-#include <Arduino.h>
 
 #include "motor.h"
 #include "pins.h"
 #include "bluetooth.h"
 
-#include <SoftwareSerial.h>
-
-SoftwareSerial bluetooth(6, 7);
-#define BLUETOOTH_BAUD 38400
-
 int main()
 {
 	sei();
 
-	// initialise_motors();
-	// start_motors();
-
-	// initialise_bluetooth();
-
-	// DDRB = 0xff;
-	// PORTB = 0xff;
-	// _delay_ms(100);
-
-	Serial.begin(9600);
-	Serial.print("Sketch:   ");
-	Serial.println(__FILE__);
-	Serial.print("Uploaded: ");
-	Serial.println(__DATE__);
-	Serial.println(" ");
-
-	bluetooth.begin(BLUETOOTH_BAUD);
-	Serial.print("BTserial started at ");
-	Serial.println(BLUETOOTH_BAUD);
-	Serial.println(" ");
-
-	char c;
-	boolean NL = true;
+	initialise_bluetooth();
 
 	while (1)
-	{
-		// Read from the Bluetooth module and send to the Arduino Serial Monitor
-		if (bluetooth.available())
-		{
-			c = bluetooth.read();
-			Serial.write(c);
-		}
-
-		// Read from the Serial Monitor and send to the Bluetooth module
-		if (Serial.available())
-		{
-			c = Serial.read();
-			bluetooth.write(c);
-		}
-	}
+		;
 }
