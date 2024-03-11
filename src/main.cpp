8a748b37fbf2f14da47947e81a1c4b4a14318f0b
Talal Habbal <talal-habbal@hotmail.com>
2024-03-11 15:46:14 +0100

    3 sensor calibration

--- src/main.cpp
+++ src/main.cpp
@@ -11,32 +11,22 @@
 #include <stdint.h>
 
 PIDController pidcontroller;
-float Kp = 1.0f, Ki = 0.00f, Kd = 0.0f;
+float Kp = 10.0f, Ki = 0.00f, Kd = 0.0f;
 float Hz = 10.0f;
 
 int main()
 {
-	DDRB |= (1 << PINB2);
 	sei();
 	initialise_motors();
+	IR_InitialiseSensor();
 	Initialize_UltrasonicSensor();
-	start_motors();
-	drive_motors(0.5f, 0.0f);
-	while (1) {
-		// Acceptance test for the sensor required.
-		// This code will be used for the acceptance test.
-		uint16_t distance = measureDistance();
-		if(distance < 10) {
-			cut_motors();
-			SetError();
-		}
-		else {
-			start_motors();
-			ClearError();
-		}
-		_delay_ms(100);
-	}
+	Bluetooth_Initialise();
+	IR_CalibrateSensors(&IR_min, &IR_max);
+	PID_Timer_Init();
+	PID_Start();
+	while(1) {
 
+	};
 	return 0;
 }
 
@@ -45,6 +35,5 @@ ISR(TIMER1_COMPA_vect)
 {
 	diagnostics.IR = IR_GetScaledValue(&IR_min, &IR_max);
 	diagnostics.PID = PIDController_Compute(&pidcontroller, 0.5f, diagnostics.IR);
-
-	// Bluetooth_Send(&diagnostics, sizeof(diagnostics));
+	Bluetooth_Send(&diagnostics, sizeof(diagnostics));
 }
